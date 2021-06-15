2ab10b5ba397692d4785db8e3bcd38de10ef3d6a
Matias Godoy <matigodoy93@hotmail.com>
2021-06-15 18:53:01 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-
+// https://superheroapi.com/api/1132055343928952
 function App() {
   return (
     <div className="App">
-      <ul class="nav nav-tabs bg-primary">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Link 1</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link 2</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link 3</a>
-        </li>
-    </ul>  
+      
     </div>
   );
 }
