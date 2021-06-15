7a5c6921325b51a3370d398fae6278d9ca447a4d
Matias Godoy <matigodoy93@hotmail.com>
2021-06-15 18:49:19 -0300

    Alkemy framework, axios and Formik

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@
 function App() {
   return (
     <div className="App">
-      
+      <ul class="nav nav-tabs bg-primary">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Link 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 3</a>
+        </li>
+    </ul>  
     </div>
   );
 }
