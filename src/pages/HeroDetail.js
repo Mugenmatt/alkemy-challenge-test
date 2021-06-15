bf71c190acf2e6efa77dc3df0291f9e913e394f5
Matias Godoy <matigodoy93@hotmail.com>
2021-06-15 18:58:26 -0300

    files and folders

---
+++ src/pages/HeroDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroDetail = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeroDetail;
---
+++ src/pages/SearchHero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchHero = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SearchHero;
