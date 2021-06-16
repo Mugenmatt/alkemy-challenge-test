ce03cae7dc87838fb993d86d3fd22a939aff7ddc
Matias Godoy <matigodoy93@hotmail.com>
2021-06-16 02:15:27 -0300

    Login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../../components/Login/Form/Form'
+
+const Login = () => {
+    return (
+        <>
+            <Form />
+        </>
+    );
+};
+
+export default Login;
