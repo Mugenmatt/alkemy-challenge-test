5beea25f0b892853399225b0254fec99fed8eed5
Matias Godoy <matigodoy93@hotmail.com>
2021-06-16 02:16:47 -0300

    Loader Component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Utilizado para otorgar un feedback al usuario mientras se está procesando una petición. Para renderizar el loader, agregar la clase alkemy-spinner a un elemento.
+
+const Loader = () => {
+    return (
+        <div class="alkemy-spinner"></div>
+    );
+};
+
+export default Loader;
