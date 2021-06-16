82d3b2c249fd5469df3728c942b110faf9f85ca8
Matias Godoy <matigodoy93@hotmail.com>
2021-06-16 02:17:42 -0300

    Login Component

---
+++ src/components/Login/Form/Form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css'
+const Form = () => {
+    return (
+        <>
+        
+            <form className='form'>
+
+                <div>
+                    <label for="email" className="alkemy-form-label">Email</label>
+                    <input type="email" className="alkemy-form-control" id="email" placeholder="Type your email" />
+                </div>
+
+                <div>
+                    <label for="password" className="alkemy-form-label">Password</label>
+                    <input type="password" className="alkemy-form-control" id="password" placeholder="Type your password" />
+                </div>
+
+                <button type="button" className="alkemy-btn-primary">Login</button>
+                
+            </form>
+
+        </>
+    );
+};
+
+export default Form;
